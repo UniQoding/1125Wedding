00cd79067c368954031aa6bc1927b4cb8d4d0d2d
Sentiboy <jinhs88@gmail.com>
2017-08-22 23:09:23 +0900

    로딩 속도 개선

--- js/image.js
+++ js/image.js
@@ -5,8 +5,13 @@ function loadImage() {
     var obj = [];
     for(var i = 1; i <= size; i++){
         obj.push({
-            imageUrl : "img/gallery/" + i + ".jpg"
+            imageUrl : "img/gallery/" + i + ".jpg",
+            imageId : i
         });
     }
-    $('#gallery > div.container-fluid > div.row-fluid > div > div#lightbox').loadTemplate('img/image.html', obj)
+
+    $('#gallery > div.container-fluid > div.row-fluid > div > div#lightbox').loadTemplate('img/image.html', obj);
+    for(var i = 1; i <= size; i++){
+        $('#'+i).lazyload();
+    }
 }
